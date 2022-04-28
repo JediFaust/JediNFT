6257f2246270e4226dfe4ba14e9cacefb97db95f
Omur <83595409+JediFaust@users.noreply.github.com>
2022-04-28 16:24:37 +0600

    Update Jedi1155.sol

--- contracts/Jedi1155.sol
+++ contracts/Jedi1155.sol
@@ -2,18 +2,20 @@
 
 pragma solidity ^0.8.4;
 
-
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 
 
-contract Jedi1155 is ERC1155Holder {
-    uint256 number;
 
-    function store(uint256 num) public {
-        number = num;
+contract Jedi1155 is ERC1155, ERC1155Holder {
+    uint256 public constant GOLD = 0;
+
+    constructor() ERC1155("https://game.example/api/item/{id}.json") {
+        _mint(msg.sender, GOLD, 10**18, "");
     }
 
-    function retrieve() public view returns (uint256){
-        return number;
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, ERC1155Receiver) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
+    
 }
